30436af1f1a9fe4da12e042fa7d10930fc8c8d05
shrutigunda <shrutigunda@cca.edu>
2017-10-29 10:10:35 -0700

    description

--- csstypeface/script.js
+++ csstypeface/script.js
@@ -1,4 +1,34 @@
 $(document).ready(function() {
+
+$(".name, #dotscroll").mouseenter(function(){
+    $(".small").css({
+        "width": "25px",
+        "height": "25px",
+        "transform": "translate(0px, 0px)",
+        "transition": "1s"
+        });
+    $(".large").css({
+        "width": "10px",
+        "height": "10px",
+        "transform":"translate(7.5px, 7.5px)",
+        "transition": "1s"
+        });
+    });
+$(".name, #dotscroll").mouseleave(function(){
+    $(".small").css({
+        "width": "10px",
+        "height": "10px",
+        "transform": "translate(7.5px, 7.5px)",
+        "transition": "1s"
+        });
+    $(".large").css({
+        "width": "25px",
+        "height": "25px",
+        "transform":"translate(0px, 0px)",
+        "transition": "1s"
+        });
+    });
+
 $(".letter").mouseenter(function(){
     $(".small1").css({
         "width": "50px",
@@ -25,13 +55,13 @@ $(".letter").mouseenter(function(){
         "transform":"translate(15px, 15px)",
         "transition": "1s"
         }); 
-	$(".large2").css({
+    $(".large2").css({
         "width": "20px",
         "height": "20px",
         "transform":"translate(15px, 15px)",
         "transition": "2s"
         }); 
-	$(".large3").css({
+    $(".large3").css({
         "width": "20px",
         "height": "20px",
         "transform":"translate(15px, 15px)",
