0ed9e4fb32807273f90fc6463a5ac373eb48b55e
shrutigunda <shrutigunda@cca.edu>
2017-12-13 08:34:47 -0800

    skrollr

--- Exhibition/script.js
+++ Exhibition/script.js
@@ -116,4 +116,29 @@ $(function(){
 
 
 
+
+
+// setSkrollr function extracted from https://www.pingdom.com/2013/
+var setSkrollr = function($el, data) {
+    for (var i = 0, l = data.length; i < l; i++) {
+        var d = data[i],
+            px = d[0];
+            css = d[1];
+        $el.attr('data-' + px, css);
+    }
+}
+
+jQuery(function($) {
+    setSkrollr($('#artist'), [ [0, 'transform:translateY(-0%)'], [6100, 'transform:translateY(-3400%)']]);
+    setSkrollr($('#name'), [ [0, 'transform:translateY(100%)'], [6100, 'transform:translateY(3500%)']]);
+    
+    skrollr.init({
+        smoothScrolling: false
+    });
+});
+
+
+
+
+
 });
